ea7f62548cdf8eb1e56e36ca9269e7f387488860
Harsh Mishra <harshmishra9933@gmail.com>
2025-08-20 01:18:19 +0530

    Service Registery

--- inventory-service/src/main/java/com/sample/microservices/inventory/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/sample/microservices/inventory/InventoryServiceApplication.java
@@ -2,7 +2,9 @@ package com.sample.microservices.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InventoryServiceApplication {
 
--- order-service/src/main/java/com/sample/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/sample/microservices/order/client/InventoryClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.ResponseEntity;
 
-@FeignClient(value = "inventory", url = "http://localhost:8082")
+@FeignClient(name = "inventory-service")
 public interface InventoryClient {
 
     @RequestMapping(method = RequestMethod.GET , value = "/api/inventory")
